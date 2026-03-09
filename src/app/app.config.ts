b2508fda84473da7f5fc748e8ac3ae98bdf3c789
connorklose12 <connorklose12@gmail.com>
2026-03-09 18:56:38 -0500

    Update appConfig to include zone change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
+
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration(withEventReplay()),
+    provideRouter(routes, withHashLocation()),  // <-- add withHashLocation()
+  ],
 };
+
