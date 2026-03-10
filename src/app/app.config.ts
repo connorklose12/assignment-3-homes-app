1f94ed1d4f5a2b5664575fcf17ec0bf63fe3d70b
connorklose12 <connorklose12@gmail.com>
2026-03-09 19:00:39 -0500

    Refactor appConfig to remove unnecessary imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,10 @@
-
-
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration(withEventReplay()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),  // <-- add withHashLocation()
   ],
 };
-
